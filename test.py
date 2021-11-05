ba99006006c4ac3a2eec05bf32bf1a19e34642bb
ursueugen <ursu_eugen@hotmail.com>
2021-11-05 11:56:07 +0200

    Refactor

--- test.py
+++ test.py
@@ -1,32 +1,45 @@
 from typing import List
 import numpy as np
 import logging
+import unittest
 
 import Bio
 import Bio.PDB
 import Bio.SVDSuperimposer
 
-from utils import *
+import utils
 
 
-parser = Bio.PDB.PDBParser(PERMISSIVE=0)  # strict parser
-structure1 = parser.get_structure("test1", "data/SAbDab-Fv-unbound/chothia/1ad0.pdb")
-structure2 = parser.get_structure("test2", "data/SAbDab-Fv-unbound/chothia/1fgv.pdb")
+class TestRMSD(unittest.TestCase):
+    
+    def test_rmsd_computation(self):
+        parser = Bio.PDB.PDBParser(PERMISSIVE=0)  # strict parser
+        structure1 = parser.get_structure("test1", "data/SAbDab-Fv-unbound/chothia/1ad0.pdb")
+        structure2 = parser.get_structure("test2", "data/SAbDab-Fv-unbound/chothia/1fgv.pdb")
 
-h_res_1, l_res_1 = extract_variable_regions_residues(structure1, "B", "A")
-h_res_2, l_res_2 = extract_variable_regions_residues(structure2, "H", "L")
+        h_res_1, l_res_1 = utils.extract_variable_regions_residues(structure1, "B", "A")
+        h_res_2, l_res_2 = utils.extract_variable_regions_residues(structure2, "H", "L")
 
-coord_h1 = extract_calpha_coord(h_res_1)
-coord_l1 = extract_calpha_coord(l_res_1)
-coord_h2 = extract_calpha_coord(h_res_2)
-coord_l2 = extract_calpha_coord(l_res_2)
+        coord_h1 = utils.extract_calpha_coord(h_res_1)
+        coord_l1 = utils.extract_calpha_coord(l_res_1)
+        coord_h2 = utils.extract_calpha_coord(h_res_2)
+        coord_l2 = utils.extract_calpha_coord(l_res_2)
 
-sup = Bio.SVDSuperimposer.SVDSuperimposer()
-rmsd_h12 = compute_rmsd(coord_h1, coord_h2, sup)
-rmsd_l12 = compute_rmsd(coord_l1, coord_l2, sup)
-rmsd_v12 = compute_rmsd(
-    np.vstack([coord_h2, coord_l2]),
-    np.vstack([coord_h1, coord_l1]),
-)
+        sup = Bio.SVDSuperimposer.SVDSuperimposer()
+        rmsd_h12 = utils.compute_rmsd(coord_h1, coord_h2, sup)
+        rmsd_l12 = utils.compute_rmsd(coord_l1, coord_l2, sup)
+        rmsd_v12 = utils.compute_rmsd(
+            np.vstack([coord_h2, coord_l2]),
+            np.vstack([coord_h1, coord_l1]),
+        )
 
-print(rmsd_h12, rmsd_l12)
+        self.assertIsNotNone(rmsd_h12)
+        self.assertIsNotNone(rmsd_l12)
+
+    def test_rmsd_dataset_computation(self):
+        df = utils.get_dataset_summary()
+        self.assertIsNotNone(utils.compute_rmsd_dataset(df, num_samples=3))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
 from typing import List
 import logging
 from dataclasses import dataclass
+from pathlib import Path
 
 import numpy as np
+import pandas as pd
 
 import Bio
 import Bio.PDB
 import Bio.SVDSuperimposer
 
+def get_dataset_summary():
+    df = pd.read_csv("data/SAbDab-Fv-unbound/20210927_0887165_summary.tsv", sep='\t')
+    df.drop_duplicates(subset = ["pdb"], keep = 'first', inplace = True)
+    df.set_index("pdb", inplace = True)
+    df = df[["Hchain", "Lchain", "engineered"]].copy()
+    return df
 
 def extract_variable_regions_residues(
     structure: Bio.PDB.Structure.Structure, 
@@ -113,3 +121,80 @@ def sample_pairs(ids: set, num_pairs: int = 100) -> set:
     samples = set(map(lambda p: p.as_sorted_tuple(), samples))
     
     return samples
+
+def compute_rmsd_dataset(
+    df, 
+    num_samples=1000, 
+    pdb_dir_path=Path('data/SAbDab-Fv-unbound/chothia/')
+    ) -> pd.DataFrame:
+    '''
+    Creates a RMSD dataset by sampling 2-combinations and
+     computing RMSD between heavy and light chains.
+    '''
+    ids = set(df.index)
+    samples = sample_pairs(ids, num_pairs = num_samples)
+    # print(samples)
+
+    records = []
+    for pair in samples:
+        try:
+            name1 = pair[0]
+            name2 = pair[1]
+            path1 = pdb_dir_path / f"{name1}.pdb"
+            path2 = pdb_dir_path / f"{name2}.pdb"
+            hchain_name_1 = df.loc[name1]['Hchain']
+            lchain_name_1 = df.loc[name1]['Lchain']
+            hchain_name_2 = df.loc[name2]['Hchain']
+            lchain_name_2 = df.loc[name2]['Lchain']
+
+            parser = Bio.PDB.PDBParser(PERMISSIVE=0)  # strict parser
+            structure1 = parser.get_structure(name1, path1)
+            structure2 = parser.get_structure(name2, path2)
+
+            h_res_1, l_res_1 = extract_variable_regions_residues(structure1, hchain_name_1, lchain_name_1)
+            h_res_2, l_res_2 = extract_variable_regions_residues(structure2, hchain_name_2, lchain_name_2)
+
+            coord_h1 = extract_calpha_coord(h_res_1)
+            coord_l1 = extract_calpha_coord(l_res_1)
+            coord_h2 = extract_calpha_coord(h_res_2)
+            coord_l2 = extract_calpha_coord(l_res_2)
+
+            sup = Bio.SVDSuperimposer.SVDSuperimposer()
+            rmsd_h12 = compute_rmsd(coord_h1, coord_h2, sup)
+            rmsd_l12 = compute_rmsd(coord_l1, coord_l2, sup)
+            rmsd_v12 = compute_rmsd(
+                np.vstack([coord_h2, coord_l2]),
+                np.vstack([coord_h1, coord_l1]),
+            )
+
+            record = {
+                "pdb1": name1,
+                "pdb2": name2,
+                "rmsd_h12": rmsd_h12,
+                "rmsd_l12": rmsd_l12,
+                "engineered_1": df.loc[name1]['engineered'],
+                "engineered_2": df.loc[name2]['engineered'],
+            }
+            records.append(record)
+        except:
+            pass
+
+    res = pd.DataFrame.from_records(records)
+    return res
+
+def annotate_engineered_status(df):
+    pair_eng_classes = []
+    for _, s in df.iterrows():
+        eng_1 = bool(s['engineered_1'])
+        eng_2 = bool(s['engineered_2'])
+        
+        if eng_1 and eng_2:
+            label = "both"
+        elif eng_1 or eng_2:
+            label = "one"
+        elif not (eng_1 or eng_2):
+            label = "none"
+
+        pair_eng_classes.append(label)
+
+    df['engineered_label'] = pair_eng_classes
